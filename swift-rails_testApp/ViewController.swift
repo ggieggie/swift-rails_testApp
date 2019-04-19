8b8e3c600b3e8ad406c2513ddb888bbeaa871df4
masahiro <aisacart@gmail.com>
2019-04-19 22:29:55 +0900

    Initial Commit

---
+++ swift-rails_testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-rails_testApp
+//
+//  Created by 木下雅大 on 2019/04/19.
+//  Copyright © 2019年 miball. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
